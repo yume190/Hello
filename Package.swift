153074cff8e0498aecca3096286ffb37717622fe
yume190 <yume190@gmail.com>
2025-06-02 16:13:30 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .tartget(
+        .target(
             name: "Hello",
             dependencies: [
                 .product(name: "HelloLib", package: "HelloLib")

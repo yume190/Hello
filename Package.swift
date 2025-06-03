bb5f29798d04ec861a7a41f1733dceb95b4c07ea
yume190 <yume190@gmail.com>
2025-06-03 15:58:39 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Hello",
+    platforms: [
+        .macOS(.v14),
+        // .iOS(.v16),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,8 +16,8 @@ let package = Package(
             targets: ["Hello"]),
     ],
     dependencies: [
-//        .package(url: "https://github.com/yume190/HelloLib", from: "1.0.0")
-        .package(url: "https://github.com/yume190/HelloLib", branch: "main")
+       .package(url: "https://github.com/yume190/HelloLib", from: "1.0.0")
+        // .package(url: "https://github.com/yume190/HelloLib", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
